4aa409bd26e1185986835cf0310ddfac575387d7
samuel <legosthes@gmail.com>
2025-08-04 15:41:44 +0800

    refactor: move views to views.py

--- wretch/urls.py
+++ wretch/urls.py
@@ -1,15 +1,6 @@
 from django.urls import path
-from django.shortcuts import render
 from django.contrib import admin
-
-
-def home(request):
-    return render(request, "home.html")
-
-
-def about(request):
-    return render(request, "about.html")
-
+from .views import home, about
 
 urlpatterns = [
     path("admin/", admin.site.urls),
