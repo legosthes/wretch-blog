eb72ab41f25b528e8c3300189e1ef1cf4857654c
samuel <legosthes@gmail.com>
2025-08-05 12:19:37 +0800

    feat: add layouts and navbar

--- pages/urls.py
+++ pages/urls.py
@@ -4,9 +4,13 @@ from django.urls import path
 # 用資料夾創立模組
 from . import views
 
+
+app_name = "pages"
 # urlpatterns的名字是固定的
+# 幫每個路徑取名字，這樣以後如果有天需要改路徑，就只要改urls.py檔案裡的就好了
+# 然後html的連結使用name的標籤
 urlpatterns = [
-    path("", views.home),
-    path("about/", views.about),
-    path("contact/", views.contact),
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
 ]
