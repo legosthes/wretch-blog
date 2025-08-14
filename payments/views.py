e5d69da69122cab67db02bdc46410a937dbf48f8
samuel <legosthes@gmail.com>
2025-08-14 15:31:06 +0800

    feat: set dotenv

--- payments/views.py
+++ payments/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import braintree
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
+PRICE_LIST = {
+    "a": 10,
+    "b": 50,
+    "c": 100,
+}
+
+
+
+@login_required
 def new(request):
     plan = request.GET.get("plan")
     # 擋非預期方案，防呆
@@ -11,26 +28,44 @@ def new(request):
         plan = "a"
 
     # token
-    # key 不要上傳上去
-    gateway = braintree.BraintreeGateway(
-        braintree.Configuration(
-            environment=braintree.Environment.Sandbox,
-            merchant_id="",
-            public_key="",
-            private_key="",
-        )
-    )
-    token = gateway.client_token.generate()
+    token = gateway().client_token.generate()
 
     return render(
         request,
         "payments/new.html",
-        {
-            "plan": plan,
-            "token": token,
-        },
+        {"plan": plan, "token": token, "price": PRICE_LIST.get(plan)},
     )
 
 
+@login_required
+@require_POST
 def index(request):
-    pass
+    nonce = request.POST.get("nonce")
+    plan = request.POST.get("plan")
+
+    result = gateway().transaction.sale(
+        {
+            "amount": PRICE_LIST.get(plan),
+            "payment_method_nonce": nonce,
+        }
+    )
+
+    if result.is_success:
+        messages.success(request, "Purchase successful.")
+    else:
+        messages.error(request, "Purchase failed, please try again.")
+
+    return redirect("articles:index")
+
+
+def gateway():
+    # key 不要上傳上去
+    return braintree.BraintreeGateway(
+        braintree.Configuration(
+            environment=braintree.Environment.Sandbox,
+            # 這裡就是用env檔案帶進來，不會hard code在這裡，也比較安全
+            merchant_id=os.getenv("merchant_id"),
+            public_key=os.getenv("public_key"),
+            private_key=os.getenv("private_key"),
+        )
+    )
--- src/scripts/braintree.js
+++ src/scripts/braintree.js
@@ -1,18 +1,19 @@
 import dropin from "braintree-web-drop-in";
 
-console.log(dropin.create);
+console.dir(dropin.create);
 
 const BrainTreePayment = () => ({
   instance: null,
-
+  nonce: "",
   ableToSubmit: false,
 
   init: async function () {
     const token = this.$el.dataset.token;
-    console.log(this.$refs.dropin);
+    // console.log(this.$refs.dropin);
     // instance 是一個promise，但我不知道他什麼時候弄好
     // 設計等他好了，讓button從disabled變成可以按
     this.instance = await dropin.create({
+      //container 就是我刷卡的填寫單
       container: this.$refs.dropin,
       authorization: token,
     });
@@ -22,8 +23,18 @@ const BrainTreePayment = () => ({
     this.ableToSubmit = true;
   },
 
-  onSubmit() {
-    console.log(this.instance);
+  async onSubmit() {
+    // 按按鈕的那一刻，按鈕變成disabled
+    this.ableToSubmit = false;
+    const { nonce } = await this.instance.requestPaymentMethod();
+    if (nonce) {
+      this.nonce = nonce;
+      //   this.$refs.nonce.value = nonce;
+      //   把表單送出去
+      this.$nextTick(() => {
+        this.$el.submit();
+      });
+    }
   },
 });
 
